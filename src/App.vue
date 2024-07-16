b4d1678193fa23e7d0ea38d413c9389b2a30c617
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-07-16 18:07:51 +0200

    add aggiunto script di background e ui

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script>
-
-</script>
-
 <template>
-  <h1>template Vue Vite</h1>
+  <div>
+    <h1>URL Safety Checker</h1>
+    <div v-if="urlInfo">
+      <p>Current URL: {{ url }}</p>
+      <p v-if="urlInfo.isMalicious">This site is malicious!</p>
+      <p v-else>This site is safe.</p>
+    </div>
+    <button @click="checkUrl">Check URL</button>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      url: "",
+      urlInfo: null,
+    };
+  },
+  methods: {
+    async checkUrl() {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      this.url = tab.url;
+      chrome.runtime.sendMessage(
+        { action: "checkUrl", url: this.url },
+        (response) => {
+          this.urlInfo = response;
+        }
+      );
+    },
+  },
+};
+</script>
